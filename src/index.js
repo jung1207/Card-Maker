3701fba45f2ed1932f6f57cc11945f4a75361811
heonuk <jung951207@gmail.com>
2021-06-05 14:02:46 +0900

    add login component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.module.css";
 import App from "./app";
+import AuthService from "./service/auth_service";
 
+const authService = new AuthService();
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App authService={authService} />
   </React.StrictMode>,
   document.getElementById("root")
 );
